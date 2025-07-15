d486df17287d44479b2ba96cfba53dcb2c99da28
Zayan <zayan_ahmad@hotmail.com>
2025-07-15 15:20:22 -0500

    add typing to methods and implement argparse

--- video_to_ascii.py
+++ video_to_ascii.py
@@ -1,9 +1,9 @@
 import cv2
-import sys, time, os
+import sys, time, os, argparse
 
 ASCII_CHARACTERS = ".,-~:;=!*#$@"[::-1]
 
-def frame_to_ascii(frame, width=100):
+def frame_to_ascii(frame, width: int) -> str:
     height = int((frame.shape[0] / frame.shape[1]) * width * 0.55)
     resized = cv2.resize(frame, (width, height))
 
@@ -14,7 +14,7 @@ def frame_to_ascii(frame, width=100):
         ascii_frame += "\n"
     return ascii_frame
 
-def get_ascii_frames(video_file_path):
+def get_ascii_frames(video_file_path: str, width: int) -> list[str]:
     capture = cv2.VideoCapture(video_file_path)
     frames = []
 
@@ -33,16 +33,23 @@ def get_ascii_frames(video_file_path):
 
     return frames
 
-def draw_frames_in_terminal(video_file_path):
+def draw_frames_in_terminal(video_file_path: str, width: int, fps: int):
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    for frame in get_ascii_frames(video_file_path):
+    for frame in get_ascii_frames(video_file_path, width):
         sys.stdout.write('\x1b[H' + frame)  # draw frame
         sys.stdout.flush()
-        time.sleep(1 / 24)  # 24 FPS
+        time.sleep(1 / fps)  # 24 FPS
 
 def main():
-    draw_frames_in_terminal("Coffee Cup Donut.mp4")
+    parser = argparse.ArgumentParser(description="Convert video to ascii")
+    parser.add_argument("video_file_path", type=str)
+    parser.add_argument("width", type=int)
+    parser.add_argument("fps", type=int)
+
+    args = parser.parse_args()
+
+    draw_frames_in_terminal(args.video_file_path, 100, 24)
 
 if __name__ == "__main__":
     main()
